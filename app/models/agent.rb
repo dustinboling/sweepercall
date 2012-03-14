c431a5ca81f08acb1c35c8bd7a817b775277c853
alan <asebastian2@gmail.com>
2012-03-14 11:38:34 -0700

    custom validations for all active? -> singularity on all notifications

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -1,6 +1,4 @@
 class Agent < ActiveRecord::Base
-  include ActiveModel::Validations
-  validates_with NotificationSingularity
   
   require 'digest/md5'
   
@@ -12,24 +10,44 @@ class Agent < ActiveRecord::Base
   belongs_to :user
   has_many :people
   
-  
-  validates_presence_of :phone
-  validates_presence_of :first_name
-  validates_presence_of :last_name
-  
   has_many :recordings
   accepts_nested_attributes_for :recordings, :allow_destroy => true
-  
-  validates_presence_of :first_name
-  validates_presence_of :last_name
-  validates_presence_of :phone
-  validates_length_of :phone, :is => 10, :message => "Phone number must contain exactly 10 numbers: XXX-XXX-XXXX"
   
   has_many :email_notifications
   accepts_nested_attributes_for :email_notifications, :allow_destroy => true
   
   has_many :sms_notifications
   accepts_nested_attributes_for :sms_notifications, :allow_destroy => true
+  
+  validate :can_only_have_one_active_sms_notification, :can_only_have_one_active_email_notification, :can_only_have_one_active_voice_notification
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+  validates_presence_of :phone
+  validates_length_of :phone, :is => 10, :message => "Phone number must contain exactly 10 numbers: XXX-XXX-XXXX"
+  
+  def can_only_have_one_active_sms_notification
+    active_sms = self.sms_notifications.collect { |s| s.active }
+    
+    if active_sms.count(true) > 1
+      errors.add(:SMS_notificaitons, ": You can only have one active SMS notification at a time")
+    end
+  end
+  
+  def can_only_have_one_active_email_notification
+    active_email = self.email_notifications.collect { |e| e.active }
+    
+    if active_email.count(true) > 1
+      errors.add(:email_notifications, ": You can only have one active Email notification at a time")
+    end
+  end
+  
+  def can_only_have_one_active_voice_notification
+    active_recordings = self.recordings.collect { |r| r.active }
+    
+    if active_recordings.count(true) > 1
+      errors.add(:voice_recordings, ": You can only have one active Voice recording at a time")
+    end
+  end
   
   def set_uuid
     self.uuid = Digest::MD5.hexdigest("#{self.user_id}" + "#{self.first_name}" + "#{self.last_name}")
