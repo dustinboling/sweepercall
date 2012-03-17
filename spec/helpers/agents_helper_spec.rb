5daa77f01cfde0699d78525b88f887bd2a469bdd
alan <asebastian2@gmail.com>
2012-03-16 20:10:37 -0700

    spec for phone_not_verified should work in all cases now

--- spec/helpers/agents_helper_spec.rb
+++ spec/helpers/agents_helper_spec.rb
@@ -13,9 +13,9 @@ require 'spec_helper'
 describe AgentsHelper do
   describe "phone_not_verified" do 
     it "queries twilio to see if the agent's phone number is verified" do
-      @agent = Factory.build(:agent, :outgoing_phone => "7145122526")
+      @agent = Factory.build(:agent, :outgoing_phone => "000000000")
       @agent.save
-      phone_not_verified?.should == false
+      phone_not_verified?.should == true
     end
   end
   
