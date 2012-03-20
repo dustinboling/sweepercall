aaed558fd4b86425faf03588cdc02f3a134bbe4b
alan <asebastian2@gmail.com>
2012-03-20 12:20:38 -0700

    changed recording button to yellow

--- spec/requests/show_agent_profile_spec.rb
+++ spec/requests/show_agent_profile_spec.rb
@@ -6,6 +6,7 @@ describe "GET agent profile :as => agent" do
     @agent = Factory(:agent, :user_id => @user.id)
     @person = Factory(:person, :agent_id => @agent.id)
     @notification = Factory(:notification, :person_id => @person.id)
+    
     visit login_path
     fill_in "Email", :with => @user.email
     fill_in "Password", :with => '123'
@@ -29,6 +30,14 @@ describe "GET agent profile :as => agent" do
     page.should have_link("Edit your profile")
   end
   
+  it "should not show the 'create a recording' button if the agent's phone number is not verified" do
+    @agent.outgoing_phone = "(000) 000-1000"
+    @agent.save
+    
+    click_link "Account"
+    page.should have_content("You cannot make a recording without verifying your phone number.")
+  end
+  
   # agent#edit
   it "should link back to agent#show from edit" do
     click_link "Edit"
@@ -39,12 +48,14 @@ describe "GET agent profile :as => agent" do
   it "should say the phone number is not verified when it is not" do
     @agent.outgoing_phone = "(000) 000-0000"
     @agent.save
+    
     click_link "Edit"
-    page.should have_content("No")
+    page.should have_content("No (verify now!)")
   end
   
   it "should display a list of agent's voice notifications" do
     @agent.recordings.create(:agent_id => @agent.id)
+    
     click_link "Account"
     page.should have_content("#{Time.now.strftime('%d %b %Y')}")
   end
