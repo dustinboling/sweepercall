8f27f8b382ae495696490e4ab4ca8449eb37ed95
alan <asebastian2@gmail.com>
2012-03-21 09:46:58 -0700

    sendgrid v5

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,2 +1,2 @@
 # This sets up the base url to tag on urls to like so: <%= $RESOURCE_NAME_url %>
-ActionMailer::Base.default_url_options[:host] = "sweepercall.heroku.com"
+# ActionMailer::Base.default_url_options[:host] = "sweepercall.heroku.com"
