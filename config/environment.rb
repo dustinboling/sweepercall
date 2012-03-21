9db8a54a43546f0c960903bf48caabed88307664
alan <asebastian2@gmail.com>
2012-03-21 09:58:36 -0700

    sendgrid v8

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,16 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Sweeper::Application.initialize!
 
+# SendGrid Configuration
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['app2920382@heroku.com'],
+  :password       => ENV['chbgoj1m'],
+  :domain         => 'heroku.com'
+}
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.default_url_options[:host] = "sweepercall.heroku.com"
+
 
