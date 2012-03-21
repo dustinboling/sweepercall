8e4f0dd8675e675f4d02f51bb632ead4159b5c6b
alan <asebastian2@gmail.com>
2012-03-20 18:57:25 -0700

    sendgrid v3

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: "mailer@sweepercall.com"
+  default from: "mailer@sweepercall.heroku.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -3,7 +3,7 @@ ActionMailer::Base.smtp_settings = {
   :port           => '587',
   :authentication => :plain,
   :user_name      => ENV['app2920382@heroku.com'],
-  :password       => ENV[''],
+  :password       => ENV['QCserqcMp3w9KszK*AtBx7uQhw'],
   :domain         => 'heroku.com'
 }
 ActionMailer::Base.delivery_method = :smtp
