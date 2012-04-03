9f262826d84260af5453a03a3766784489403b4c
alan <asebastian2@gmail.com>
2012-04-02 19:48:33 -0700

    Fixed agent email on outgoing emailer

--- app/mailers/sweep_mailer.rb
+++ app/mailers/sweep_mailer.rb
@@ -6,12 +6,14 @@ class SweepMailer < ActionMailer::Base
     @person = person
     
     # set the agent's email as "from", if applicable
-    if @person.agent_id.blank?
+    if @person.agent_id == 12 
+      @from = "mailer@sweepercall.com"
+    elsif @person.agent_id == nil
       @from = "mailer@sweepercall.com"
     else
       @from = Agent.find(@person.agent_id).outgoing_email
     end
-    
+
     # send the mail off  
     mail(:to => Person.find(@person.id).email,
          :from => @from,
--- spec/mailers/sweep_mailer_spec.rb
+++ spec/mailers/sweep_mailer_spec.rb
@@ -1,5 +1,46 @@
 require "spec_helper"
 
 describe SweepMailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "sweep_notification (with agent assignment)" do
+    let(:user) { Factory(:user, :email => "agents_email@example.com") }
+    let(:agent) { Factory(:agent, :user_id => user.id, :outgoing_email => user.email, :id => 999) }
+    
+    let(:person) { Factory(:person, :email => "test7@dustinboling.com", :agent_id => 999) }
+    let(:mail) { SweepMailer.sweep_notification(person, agent) }
+
+    it "sends a sweep notification" do
+      mail.subject.should eq("SweeperCall Notification")
+      mail.to.should include(person.email)
+    end
+    
+    it "says it is coming from the agent's email when an agent is assigned" do
+      mail.from.should include(User.find(agent.user_id).email) 
+    end
+
+
+    it "includes an agent message when an agent is assigned to the person" do
+      # not doing this at the moment
+    end
+
+  end
+
+  describe "sweep_notification (without agent assignment)" do
+    let(:user) { Factory(:user) }
+    let(:person) { Factory(:person, :user_id => user.id, :agent_id => 12, :email => "test7@dustinboling.com") }
+    let(:agent) { Factory(:agent, :id => 12) }
+    let(:mail) { SweepMailer.sweep_notification(person, agent) }
+
+    it "sends a sweep notification" do
+      mail.subject.should eq("SweeperCall Notification")
+      mail.to.should include(person.email)
+    end
+
+    it "says it is coming from mailer@sweepercall.com when it is coming from us" do
+      mail.from.should include("mailer@sweepercall.com")
+    end
+
+    it "does not include agent message when an agent is not assigned (agent_id = 12)" do
+      # not doing this at the moment
+    end
+  end
 end
